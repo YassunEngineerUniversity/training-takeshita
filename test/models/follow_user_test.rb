c8e003dc7ff4a4972025b9be7007499a641fb281
raithend <37509998+raithend@users.noreply.github.com>
2024-11-30 20:53:42 +0900

    Use annotate to add schema information

--- test/models/follow_user_test.rb
+++ test/models/follow_user_test.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: follow_users
+#
+#  id          :integer          not null, primary key
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  followee_id :integer
+#  follower_id :integer
+#
+# Indexes
+#
+#  index_follow_users_on_followee_id                  (followee_id)
+#  index_follow_users_on_follower_id                  (follower_id)
+#  index_follow_users_on_follower_id_and_followee_id  (follower_id,followee_id) UNIQUE
+#
 require 'test_helper'
 
 class FollowUserTest < ActiveSupport::TestCase
