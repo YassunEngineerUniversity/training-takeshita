39088324edc6ba1add9ea60681c2201caff7744b
raithend <37509998+raithend@users.noreply.github.com>
2024-12-05 22:28:12 +0900

    Create DELETE /api/users/{:id}/follow api

--- spec/requests/follow_users_api_spec.rb
+++ spec/requests/follow_users_api_spec.rb
@@ -24,12 +24,27 @@ RSpec.describe 'Api::FollowUsers', type: :request do
       post '/api/login', params: { session: { email: current_user.email, password: 'password' } }
     end
     context 'フォロー成功' do
-      it '投稿の作成に成功すること' do
+      it 'フォローに成功すること' do
         expect do
           post "/api/users/#{another_user.id}/follow"
         end.to change(current_user.followees, :count).by(1)
-        binding.pry
         expect(response).to have_http_status(:created)
+      end
+    end
+  end
+
+  describe 'DELETE /api/users/#{another_user.id}/follow' do
+    # ログイン、フォロー処理
+    before do
+      post '/api/login', params: { session: { email: current_user.email, password: 'password' } }
+      post "/api/users/#{another_user.id}/follow"
+    end
+    context 'フォロー解除成功' do
+      it 'フォローの解除に成功すること' do
+        expect do
+          delete "/api/users/#{another_user.id}/follow"
+        end.to change(current_user.followees, :count).by(-1)
+        expect(response).to have_http_status(:see_other)
       end
     end
   end
