befacdff304798fff476dfae4924f0ba11e8eaef
raithend <37509998+raithend@users.noreply.github.com>
2024-12-05 20:35:29 +0900

    Create a GET /api/posts/{id} api

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -3,6 +3,20 @@ module Api
     before_action :logged_in_user, only: %i[index show create destroy]
     before_action :correct_user,   only: :destroy
 
+    def show
+      # パラメータからidを取得
+      @post = Post.find(params[:id])
+
+      # JSONレスポンスを返す
+      render json: {
+        content: @post.content,
+        created_at: @post.created_at
+      }, status: :ok
+    rescue ActiveRecord::RecordNotFound
+      # レコードが見つからない場合
+      render json: { error: 'Post not found' }, status: :not_found
+    end
+
     def create
       @post = current_user.posts.build(post_params)
       @post.image.attach(params[:post][:image])
